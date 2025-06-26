79bf33f04a5b358834909bcc11cd2da8ea7f090f
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-26 06:55:17 -0300

    Starting Project

---
+++ src/main/java/io/githun/Guimaraes131/cashflowTrackerApi/CashflowTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package io.githun.Guimaraes131.cashflowTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/githun/Guimaraes131/cashflowTrackerApi/CashflowTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.githun.Guimaraes131.cashflowTrackerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
